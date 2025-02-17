689206347bbb9f97559b40992978fd010b5efe3b
Madhavi-Patil <88056234+Madhavi-Patil@users.noreply.github.com>
2025-02-17 07:15:11 -0500

    Update tasks.js
    + comments

--- src/tasks.js
+++ src/tasks.js
@@ -1,14 +1,16 @@
-
+// This function was created to return a new "cleaned-up" string with any non-numeric characters
+// removed. For example:
+// an input of: "7-623" should return: "7623"
+// and an input of: "..2965a" should return "2965"
 function getDigits(inputString){
-    //console.log('inputString' + inputString);
    return(inputString.replace(/[^0-9]/g,''));
 }
 
+// This function was created to return a new list with the
+// same elements in the same order, but removing duplicates and without changing the original list.
+// All elements are of the same type in the Original list.
 function uniqueItems(inputItems){
-    /* var uniq = [ ...new Set(inputItems) ];
-    console.log(' org list:' + inputItems + ' new list:' + uniq );
-    return uniq; */
-   
+    
     var seen = {};
     var out = [];
     var len = inputItems.length;
@@ -24,6 +26,12 @@ function uniqueItems(inputItems){
   return out;
 }
 
+
+// This function was created to return a list
+// of specialty names, without duplicates, that are represented by 
+// the given IDs (after being "cleaned up" by the process from getDigits()).
+// Note: there are no duplicates in the list of specialties: no ID will appear for multiple
+// specialty areas.
 function getUniqueSpecialties(ids, specialities){
 
     // 1. Make sure list[ids] not Empty
@@ -31,19 +39,15 @@ function getUniqueSpecialties(ids, specialities){
     {
         return "";
     }
-   // console.log("Ids: " + ids);
 
     // 2. Extract numeric keys/ ids
     var numericIds =[];
     ids.forEach(element => {
         numericIds.push(getDigits(element));
     }); 
-    //console.log("Numeric ids: " + numericIds);
 
     // 3. Get list of unique ids / exclude duplicates from ids
     var uniqueIds = uniqueItems(numericIds);
-    // console.log ("Unique ids: " + uniqueIds);
-    // console.log("Spe " + specialities.length);
     
     // 4. Find respective Speciality for each id
     var listOfSpecialities = [];
@@ -52,7 +56,6 @@ function getUniqueSpecialties(ids, specialities){
         const potential = specialities.find(([key]) => key.toString() === element);
         if(potential) { listOfSpecialities.push(potential[1]);};
     });
-    // console.log(listOfSpecialities);
     return(listOfSpecialities);
     
 }
